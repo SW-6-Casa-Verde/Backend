fbad034289e93daaf5253cd6fbdb3f70ef7209be
LeeJinIe <jj0745@naver.com>
2023-10-03 11:04:29 +0900

    Feat: item 스키마 추가

---
+++ db/schemas/item.js
@@ -0,0 +1,37 @@
+const { Schema } = require('mongoose');
+
+const PostSchema = new Schema({
+  item_id: {
+    type: Number,
+    required: true,
+  },
+  item_name: {
+    type: String,
+    required: true,
+  },
+  item_price: {
+    type: Number,
+    required: true,
+  },
+  item_description: {
+    type: String,
+    required: true,
+  },
+  item_main_image: { //대표 이미지
+    type: [ String ],
+    required: true,
+  },
+  item_images: [ String ], // 상세 이미지
+  category_id: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+  user_id: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  }
+})
+
+module.exports = PostSchema;
