ebfa7f50459d3b290f572260cb9953ab2c23b23b
LeeJinIe <jj0745@naver.com>
2023-10-04 22:36:12 +0900

    Feat: Category 모델 수정

--- db/models/Category.js
+++ db/models/Category.js
@@ -4,49 +4,49 @@ import { CategorySchema } from "../schemas/category";
 const CategoryModel = model("Category", CategorySchema);
 
 class Category {
-    // Create
-    static async create({ newCategory }) {
-        const category = CategoryModel.insertOne({ newCategory });
-        return category;
-    }
+  // Create
+  static async create({ newCategory }) {
+    const category = CategoryModel.insertOne({ newCategory });
+    return category;
+  }
 
-    // Read
-    static async findAll() {
-        const categories = CategoryModel.find({});
-        return categories;
-    }
-    
-    static async findById( id ) {
-        const category = CategoryModel.findOne({ id });
-        return category;
-    }
+  // Read
+  static async findAll() {
+    const categories = CategoryModel.find({});
+    return categories;
+  }
 
-    static async findByName( name ) {
-        const category = CategoryModel.findOne({ name });
-        return category;
-    }
+  static async findById(id) {
+    const category = CategoryModel.findOne({ id });
+    return category;
+  }
 
-    // Update
-    static async updateById({ id }, { query }) {
-        const category = CategoryModel.findOneAndUpdate({ id }, { query });
-        return category;
-    }
+  static async findByName(name) {
+    const category = CategoryModel.findOne({ name });
+    return category;
+  }
 
-    static async updateByName({ name }, { query }) {
-        const category = CategoryModel.findOneAndUpdate({ name }, { query });
-        return category;
-    }
+  // Update
+  static async updateById({ id }, { query }) {
+    const category = CategoryModel.findOneAndUpdate({ id }, { query });
+    return category;
+  }
 
-    // Delete
-    static async deleteById({ id }, { query }) {
-        const category = CategoryModel.findOneAndDelete({ id }, { query });
-        return category;
-    }
+  static async updateByName({ name }, { query }) {
+    const category = CategoryModel.findOneAndUpdate({ name }, { query });
+    return category;
+  }
 
-    static async deleteByIdByName({ name }, { query }) {
-        const category = CategoryModel.findOneAndDelete({ name }, { query });
-        return category;
-    }
+  // Delete
+  static async deleteById({ id }, { query }) {
+    const category = CategoryModel.findOneAndDelete({ id }, { query });
+    return category;
+  }
+
+  static async deleteByIdByName({ name }, { query }) {
+    const category = CategoryModel.findOneAndDelete({ name }, { query });
+    return category;
+  }
 }
 
-export { Category }
+export { Category };
