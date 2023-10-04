917199f8871843dff84e83486b9033e1835c06a4
LeeJinIe <jj0745@naver.com>
2023-10-04 22:38:58 +0900

    Style: tab size 4->2 변경

--- db/models/Item.js
+++ db/models/Item.js
@@ -5,20 +5,55 @@ import { CategorySchema } from "../schemas/category";
 const ItemModel = model("Item", ItemSchema);
 
 class Item {
-  static async findById({ item_id }) {
-    const item = await ItemModel.findOne({ id: item_id });
+  // CREATE
+  static async create({ newItem }) {
+    const createdNewItem = await ItemModel.insertOne({ newItem });
+    return createdNewItem;
+  }
+
+  // READ
+  static async findByCategory({ category_id }, sortQuery, page, perPage) {
+    const category = await CategorySchema.findOne({ id: category_id });
+    const itemList = await ItemModel.find({ category_id: category }).populate(
+      "category"
+    );
+
+    // 정렬
+    if (sortQuery) itemList = await itemList.sort(sortQuery);
+
+    // pagination
+    if (page && perPage) {
+      const [total, items] = await Promise.all([
+        ItemModel.countDocuments({}),
+        itemList.skip(perPage * (page - 1)).limit(perPage),
+      ]);
+
+      const totalPage = Math.ceil(total / perPage);
+
+      return [items, totalPage];
+    }
+
+    // pagenation 필요 없을 떄
+    return [itemList, null];
+  }
+
+  static async findById(id) {
+    const item = await ItemModel.findOne({ id }).populate("category");
     return item;
   }
 
-  static async findAll() {
-    const items = await ItemModel.find({});
-    return items;
+  // UPDATE
+  static async updateById(id, query) {
+    const item = await ItemModel.findOneAndUpdate({ id }, query).populate(
+      "category"
+    );
+    return item;
   }
 
-  static async findByCategory({ category_id }) {
-    const category = await CategorySchema.findOne({ id: category_id });
-    const items = await ItemModel.find({ category_id: category }).populate('');
-    return items;
+  // DELETE
+  static async deleteById(id) {
+    const item = await ItemModel.delete({ id }).populate("category");
+    return item;
   }
 }
 
