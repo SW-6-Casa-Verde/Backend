7d59634c940b306926b6c739b5005a5fba3c9662
LeeJinIe <jj0745@naver.com>
2023-10-04 11:35:54 +0900

    Feat: item 스키마 필드 이름 변경 및 sales, timestamps 필드 추가

--- db/schemas/item.js
+++ db/schemas/item.js
@@ -15,16 +15,22 @@ const ItemSchema = new Schema({
         type: String,
         required: true,
     },
-    main_image: { //대표 이미지
+    main_image: { // 대표 이미지
         type: [ String ],
         required: true,
     },
-    images: [ String ], // 상세 이미지
-    category_id: { // id만 가져오는 게 아니라 오브젝트 자체를 가져오니까 그냥 "category"가 맞을 것 같아요!
+    images: [ String ], // 상세 이미지(옵션)
+    category: { // id만 가져오는 게 아니라 오브젝트 자체를 가져오니까 그냥 "category"가 맞을 것 같아요!
         type: Schema.Types.ObjectId,
         ref: 'Category',
         required: true,
+    },
+    sales: {
+      type: Number,
+      default: 0,
     }
+}, {
+    timestamps: true,
 })
 
 module.exports = ItemSchema;
