a97ea167a2cd6e3438713be56413ec0f3c9f44e4
LeeJinIe <jj0745@naver.com>
2023-10-05 15:12:21 +0900

    Feat: item router-service 연결 추가 및 itemService 수정
    - item get, post, put, delete 요청 추가
    - item service에서 item 상세 가져오기 'getItem' 추가
    - postman 테스트 후 수정 예정

--- routes/items.js
+++ routes/items.js
@@ -1,8 +1,113 @@
-import express from "express";
-const router = express.Router();
+import { Router } from "express";
+import { itemService } from "../services/itemService";
+import asyncHandler from "../utils/asyncHandler";
 
-router.get("/", (req, res) => {
-  res.send();
-});
+const itemRouter = Router();
 
-module.exports = router;
+itemRouter.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const page = req.query.page || 1;
+    const perPage = req.query.perPage || 10;
+    const sort = req.query.sort;
+    const category = req.category;
+
+    // sort query 설정
+    if (!sort || sort === "최신순") {
+      sort = { created_at: -1 };
+    } else if (sort === "인기순") {
+      sort = { sales: -1 };
+    } else {
+      throw { status: 400, message: "잘못된 요청입니다. 요청한 값을 다시 확인해주세요." };
+    }
+
+    // 전체 상품 조회
+    if (!category && !sort) {
+      const items = await itemService.getItems(page, perPage, sort);
+
+      if (items.errorMessage) {
+        throw { status: 404, message: items.errorMessage };
+      }
+
+      res.status(200).json({ message: "success", items });
+    }
+
+    // 카테고리별 상품 조회
+    if (category && !sort) {
+      const items = await itemService.getItemsByCategory({ category }, page, perPage, sort);
+
+      if (items.errorMessage) {
+        throw { status: 404, message: items.errorMessage };
+      }
+
+      res.status(200).json({ message: "success", items });
+    }
+  })
+);
+
+itemRouter.get(
+  "/:item_id",
+  asyncHandler(async (req, res) => {
+    const { item_id } = req.params;
+
+    const item = await itemService.getItem({ item_id });
+
+    if (item.errorMessage) {
+      throw { status: 404, message: item.errorMessage };
+    }
+
+    res.status(200).json({ message: "success", item });
+  })
+);
+
+itemRouter.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    const { name, price, description, main_image, images, category } = req.body;
+
+    if (!name || !price || !description || !main_image || !category) {
+      throw { status: 400, message: "잘못된 요청입니다. 요청한 값을 다시 확인해주세요." };
+    }
+
+    const item = await itemService.addItem({ name, price, description, main_image, images, category });
+
+    res.status(201).json({ message: "success", item });
+  })
+);
+
+itemRouter.put(
+  "/:item_id",
+  asyncHandler(async (req, res) => {
+    const { item_id } = req.params;
+    const { name, price, description, main_image, images, category } = req.body;
+
+    if (!name || !price || !description || !main_image || !category) {
+      throw { status: 400, message: "잘못된 요청입니다. 요청한 값을 다시 확인해주세요." };
+    }
+
+    const item = await itemService.setItem({ item_id }, { name, price, description, main_image, images, category });
+
+    if (item.errorMessage) {
+      throw { status: 404, message: item.errorMessage };
+    }
+
+    res.status(201).json({ message: "success", item });
+  })
+);
+
+itemRouter.delete(
+  "/:item_id",
+  asyncHandler(async (req, res) => {
+    const { item_id } = req.params;
+
+    const item = await itemService.deleteItem({ item_id });
+
+    if (item.errorMessage) {
+      throw { status: 404, message: item.errorMessage };
+    }
+
+    res.status(200).json({ message: "success", item });
+  })
+);
+
+export { itemRouter };
--- services/itemService.js
+++ services/itemService.js
@@ -1,4 +1,3 @@
-import { create } from "domain";
 import { Item } from "../db/models/Item"; // db/index.js 완성되면 바꾸기 -> ../db
 import { Category } from "../db/models/Category"; // db/index.js 완성되면 바꾸기 -> ../db
 
@@ -16,9 +15,9 @@ class itemService {
   }
 
   // Read
-  // 상품 1개
-  static async getItem(item_id) {
-    const item = await Item.findById(item_id);
+  // 상품 한 개
+  static async getItem({ item_id }) {
+    const item = await Item.findById({ item_id });
 
     if (!item) {
       const errorMessage = "해당 상품이 존재하지 않습니다.";
@@ -28,9 +27,9 @@ class itemService {
     return item;
   }
 
-  // 상품 전체 & pagination
-  static async getItems(page, perPage) {
-    const [items, totalPage] = await Item.findByCategory({}, page, perPage);
+  // 상품 전체 & sort
+  static async getItems(page, perPage, sortQuery) {
+    const [items, totalPage] = await Item.findByCategory({}, page, perPage, sortQuery);
 
     if (!items) {
       const errorMessage = "상품이 존재하지 않습니다.";
@@ -40,16 +39,16 @@ class itemService {
     return [items, totalPage];
   }
 
-  // 카테고리별 상품 & pagination
-  static async getItemsByCategory(category_id, page, perPage) {
-    const category = Category.findById(category_id);
+  // 카테고리별 상품 & sort
+  static async getItemsByCategory({ category_id }, page, perPage, sortQuery) {
+    const category = await Category.findById({ category_id });
 
     if (!category) {
       const errorMessage = "카테고리가 존재하지 않습니다.";
       return { errorMessage };
     }
 
-    const [items, totalPage] = await Item.findByCategory({ category }, page, perPage);
+    const [items, totalPage] = await Item.findByCategory({ category }, page, perPage, sortQuery);
 
     if (!items) {
       const errorMessage = "상품이 존재하지 않습니다.";
@@ -60,28 +59,28 @@ class itemService {
   }
 
   // Update
-  static async setItem(item_id, query) {
-    const item = await Item.findById(item_id);
+  static async setItem({ item_id }, query) {
+    const item = await Item.findById({ item_id });
 
     if (!item) {
       const errorMessage = "해당 상품이 존재하지 않습니다.";
       return { errorMessage };
     }
 
-    const updatedItem = await Item.updateById(item_id, query);
+    const updatedItem = await Item.updateById({ item_id }, query);
     return updatedItem;
   }
 
   // Delete
-  static async removeItem(item_id) {
-    const item = await Item.findById(item_id);
+  static async deleteItem({ item_id }) {
+    const item = await Item.findById({ item_id });
 
     if (!item) {
       const errorMessage = "해당 상품이 존재하지 않습니다.";
       return { errorMessage };
     }
 
-    const deletedItem = await Item.deleteById(item_id);
+    const deletedItem = await Item.deleteById({ item_id });
     return deletedItem;
   }
 }
