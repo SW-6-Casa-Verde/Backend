dddc62c7f814580878d5503344ddb30a0641dd7b
LeeJinIe <jj0745@naver.com>
2023-10-07 02:49:27 +0900

    Feat: item 스키마 이미지 필드 이름 변경

--- db/schemas/item.js
+++ db/schemas/item.js
@@ -16,17 +16,16 @@ const ItemSchema = new Schema(
       type: String,
       required: true,
     },
-    main_image: {
+    main_images: {
       // 대표 이미지
-      type: [
-        {
-          data: Buffer,
-          contentType: String,
-        },
-      ],
+      type: [String],
       required: true,
     },
-    images: [String], // 상세 이미지(옵션)
+    images: {
+      // 상세 이미지(옵션)
+      type: [String],
+      default: [],
+    },
     category: {
       // id만 가져오는 게 아니라 오브젝트 자체를 가져오니까 그냥 "category"가 맞을 것 같아요!
       type: Schema.Types.ObjectId,
