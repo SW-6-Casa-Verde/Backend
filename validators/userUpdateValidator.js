4fde59d98c94140b51cf1af9e7aff7cc3a592eca
jscht <80654820+jscht@users.noreply.github.com>
2023-10-08 23:05:50 +0900

    Fix: "유저 정보 업데이트 유효성 검사 패턴 수정"
    이름 필드 정규표현식에 한글 추가

--- validators/userUpdateValidator.js
+++ validators/userUpdateValidator.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 // const emailPattern = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
 const passwordPattern = /^(?=.*[a-zA-Z])((?=.*\d)|(?=.*\W))(?=.*[!@#$%^*+=-]).{8,15}$/;
 const phoneNumberPattern = /^\d{3}-\d{3,4}-\d{4}$/;
-const namePattern = /^[a-zA-Z0-9]{2,12}$/;
+const namePattern = /^[a-zA-Z가-힣0-9]{2,12}$/;
 
 // Joi 스키마 정의
 const userUpdateJoiSchema = Joi.object({
