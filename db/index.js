41ee6fca9fba4e095dfe715f38c06010b43bb6d5
jscht <80654820+jscht@users.noreply.github.com>
2023-10-09 21:26:33 +0900

    Add: "비회원 유저 기본 데이터 추가"

--- db/index.js
+++ db/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import bcrypt from "bcrypt";
 dotenv.config();
 import { User } from "./models/user";
+import { userRole } from "../constants";
 
 mongoose.connect(`${process.env.DATABASE_URL}/CasaVerde`);
 
@@ -14,10 +15,10 @@ mongoose.connection.on("connected", () => {
 });
 
 async function boot() {
+  const adminPassword = await bcrypt.hash(process.env.ADMIN_PASSWORD, 10);
+
   // 관리자 유저 기본 데이터
   const adminCheck = await User.findByUserId("admin_id");
-  // 관리자 데이터가 없으면 넣음.
-  const adminPassword = await bcrypt.hash(process.env.ADMIN_PASSWORD, 10);
   if (!adminCheck) {
     await User.create({
       uuid: "admin_id",
@@ -26,13 +27,23 @@ async function boot() {
       address: "서울특별시 구구 동동",
       phone: "010-0000-0000",
       name: "admin",
-      role: "ADMIN",
+      role: userRole.ADMIN,
     });
   }
 
-  // 관리자 jwt 확인
-  // const { uuid, email, password } = adminCheck;
-  // console.log(createJWT({ uuid, email, password, role: "USER" }));
+  // 비회원 유저 기본 데이터
+  const guestCheck = await User.findByUserId("guest_id");
+  if (!guestCheck) {
+    await User.create({
+      uuid: "guest_id",
+      email: "guest@guest.com",
+      password: adminPassword,
+      address: "서울특별시 구구 동동",
+      phone: "010-0000-0000",
+      name: "guest",
+      role: userRole.USER,
+    });
+  }
 }
 
 boot();
