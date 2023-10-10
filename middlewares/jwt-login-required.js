b7237d4a9e88d361272d571680d00ad48fb41369
jscht <80654820+jscht@users.noreply.github.com>
2023-10-10 20:34:20 +0900

    Fix: "미들웨어 에러 처리 부분 수정"

--- middlewares/jwt-login-required.js
+++ middlewares/jwt-login-required.js
@@ -6,20 +6,22 @@ export default function jwtLoginRequired() {
   // 쿠키 블랙리스트 등록
   async function setBlacklist(req, res, next) {
     const token = req.cookies.token;
+    if (!token) {
+      return next({ status: 401, message: "인증되지 않은 사용자" });
+    }
+
     const decode = await verifyJWT(token);
     const localBlackList = req.app.locals.blacklist;
 
     if (decode.errorMessage || localBlackList.has(token)) {
-      // 바로 돌려보낸다면?
-      // return res.status(401).redirect("/");
       localBlackList.add(token);
+      console.log(localBlackList);
       const { status, errorMessage } = decode;
-      next({ status, message: errorMessage });
+      return next({ status, message: errorMessage });
     }
 
     const { role, uuid } = decode;
     req.user = { role, uuid };
-    req.token = token;
 
     next();
   }
