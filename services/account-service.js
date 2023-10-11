4c78be580a4e87e8200d26d838a23841c0bdcbb5
LeeJinIe <jj0745@naver.com>
2023-10-12 01:42:11 +0900

    Feat: auth router 병합

--- services/account-service.js
+++ services/account-service.js
@@ -2,7 +2,7 @@ import bcrypt from "bcrypt";
 import { User } from "../db";
 
 class AccountService {
-  static async googleLogin({ email, password }) {
+  static async login({ email, password }) {
     const message = "로그인에 실패하였습니다.";
 
     const user = await User.findByEmail(email);
@@ -12,7 +12,7 @@ class AccountService {
       return { status: 401, message };
     }
 
-    const isPasswordMatch = await bcrypt.compareSync(password, user.password);
+    const isPasswordMatch = bcrypt.compareSync(password, user.password);
 
     if (!isPasswordMatch) {
       return { status: 401, message };
