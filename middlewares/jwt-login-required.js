429ee70ba162c21ff2967aa4b0c670023a784527
jscht <80654820+jscht@users.noreply.github.com>
2023-10-13 04:43:42 +0900

    Fix: "jwt 통신 로직 변경"
    일반, 소셜 로그인 시 응답 데이터에 포함.
    토큰 받을 때 headers.authorization 로 받아서 처리

--- middlewares/jwt-login-required.js
+++ middlewares/jwt-login-required.js
@@ -5,22 +5,24 @@ export default function jwtLoginRequired() {
 
   // 쿠키 블랙리스트 등록
   async function setBlacklist(req, res, next) {
-    const token = req.cookies.token;
+    const token = req.headers.authorization?.split('Bearer ')[1];
     if (!token) {
       return next({ status: 401, message: "인증되지 않은 사용자" });
     }
 
     const decode = await verifyJWT(token);
     const localBlackList = req.app.locals.blacklist;
+    if (localBlackList.has(decode?.jti)) {
+      throw { status: 401, message: "Token revoked" };
+    }
 
-    if (decode.errorMessage || localBlackList.has(token)) {
+    if (decode.errorMessage) {
       const { status, errorMessage } = decode;
-      if (errorMessage) localBlackList.add(token);
       return next({ status, message: errorMessage });
     }
 
-    const { role, uuid } = decode;
-    req.user = { role, uuid };
+    const { role, uuid, jti } = decode;
+    req.user = { role, uuid, jti };
 
     next();
   }
