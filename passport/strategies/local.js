ed2e927b4615a73dc55e9e8a369fc206b412b261
조 승준 <tmdwns489@gmail.com>
2023-10-13 05:24:04 +0900

    TEST:Token Test

--- passport/strategies/local.js
+++ passport/strategies/local.js
@@ -12,15 +12,10 @@ const local = new LocalStrategy(config, async (email, password, done) => {
     const { error, value } = await validateLogin({ email, password });
     if (error) throw { status: 400, message: "요청한 값을 다시 확인해주세요." };
 
-        const localUser = await AccountService.localLogin(value);
-        if (localUser.message) {
-            const { status, message } = localUser;
-            throw { status, message };
-        }
-        // req.user로 정보가 들어감 (이름이 user인 이름은 passport 규칙)
-        done(null, localUser);
-    } catch (error) {
-        done(error, null);
+    const localUser = await AccountService.googleLogin(value);
+    if (localUser.message) {
+      const { status, message } = localUser;
+      throw { status, message };
     }
     // req.user로 정보가 들어감 (이름이 user인 이름은 passport 규칙)
     done(null, localUser);
